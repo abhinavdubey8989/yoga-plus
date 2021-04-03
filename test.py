2434a060f106345abec72b686a48f0e5b89e84f6
abhinav_dubey_revx <abhinav.dubey@affle.com>
2021-04-03 14:48:27 +0530

    minor changes

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ y= data.Pose
 X = data.drop('Pose',axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 scaler = StandardScaler()
+
 scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
@@ -33,6 +34,9 @@ X_test = scaler.transform(X_test)
 classifier = svm.SVC()
 classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
+
+
+
 print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 print(y_pred)
